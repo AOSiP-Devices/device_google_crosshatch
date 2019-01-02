be427e5e4963a58f8925f025fbbbe7a6b3634fb6
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-01-01 22:02:22 -0500

    AOSiP-ify
    Change-Id: I85c2bc11f9bba464cdd7a19108295e836a3d9fe8
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -22,7 +22,7 @@
 #
 
 # Include DU common configuration
-include vendor/du/config/common_full_phone.mk
+include vendor/aosip/config/common_full_phone.mk
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
@@ -31,7 +31,7 @@ $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-PRODUCT_NAME := du_crosshatch
+PRODUCT_NAME := aosip_crosshatch
 PRODUCT_DEVICE := crosshatch
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel 3 XL
